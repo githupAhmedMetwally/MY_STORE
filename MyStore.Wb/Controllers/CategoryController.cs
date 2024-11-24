bf5357e358571745f1cd941e6b3d9313d520f5f5
ametwally494@gmail.com <ametwally494@gmail.com>
2024-11-24 11:28:07 +0200

    first

---
+++ MyStore.Wb/Controllers/CategoryController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyStore.Wb.Models;
+
+namespace MyStore.Wb.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext context;
+
+		public CategoryController(ApplicationDbContext context)
+        {
+			this.context = context;
+		}
+        public IActionResult Index()
+		{
+			var result = context.Category.ToList();
+			return View(result);
+		}
+		[HttpGet]
+        public IActionResult Create()
+        {
+			return View();
+        }
+        [HttpPost]
+		[ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+			if (ModelState.IsValid)
+			{
+                context.Category.Add(category);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+			return View(category);
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                NotFound();
+            }
+            var categoryId = context.Category.Find(id);
+            return View(categoryId);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Category.Update(category);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                NotFound();
+            }
+            var categoryId = context.Category.Find(id);
+            return View(categoryId);
+        }
+        [HttpPost]
+        public IActionResult DeleteCategory(int? id)
+        {
+            
+            var categoryId = context.Category.Find(id);
+            if(categoryId == null){
+                NotFound();
+            }
+           context.Category.Remove(categoryId);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MyStore.Wb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyStore.Wb.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
